049dcdfee2cb8bd3399652a02ceb00182f4de9e3
Profe Juan Mac <juanrinconada@cev.com>
2024-10-02 09:34:18 +0100

    Add number to letter conversion

--- swift-basico/main.swift
+++ swift-basico/main.swift
@@ -10,31 +10,49 @@ func tieneCaracteresEspeciales(texto: String) -> Bool {
     }
     return false
 }
-/*
-print("¿Cómo te llamas?")
-var nombre = readLine()!
 
-var nombreIncorrecto = false
+func pedirNombre() -> String {
+    print("¿Cómo te llamas?")
+    var nombre = readLine()!
 
-repeat {
-    if nombre.count > 10 { // Máximo 10 letras
-        nombreIncorrecto = true
-        print("Error: Nombre demasiado largo, escribe otro  nombre")
-        nombre = readLine()!
-    } else if nombre.first!.isNumber { // No emperar por número
-        nombreIncorrecto = true
-        print("Error: No se puede empezar con un número, escribe otro nombre")
-        nombre = readLine()!
-    } else if tieneCaracteresEspeciales(texto: nombre) { // Sin caracteres especiales
-        nombreIncorrecto = true
-        print("Error: Sólo letras y números, escribe otro  nombre")
-        nombre = readLine()!
-    } else {
-        nombreIncorrecto = false
+    var nombreIncorrecto = false
+
+    repeat {
+        if nombre.count > 10 { // Máximo 10 letras
+            nombreIncorrecto = true
+            print("Error: Nombre demasiado largo, escribe otro  nombre")
+            nombre = readLine()!
+        } else if nombre.first!.isNumber { // No emperar por número
+            nombreIncorrecto = true
+            print("Error: No se puede empezar con un número, escribe otro nombre")
+            nombre = readLine()!
+        } else if tieneCaracteresEspeciales(texto: nombre) { // Sin caracteres especiales
+            nombreIncorrecto = true
+            print("Error: Sólo letras y números, escribe otro  nombre")
+            nombre = readLine()!
+        } else {
+            nombreIncorrecto = false
+        }
+    } while nombreIncorrecto
+    
+    return nombre
+}
+
+func cambiarNumeros(nombre: String) -> String {
+    let numeros = [1: "uno", 2: "dos", 3: "tres", 4: "cuatro", 5: "cinco", 6: "seis", 7: "siete", 8: "ocho", 9: "nueve", 0: "cero"]
+    var resultado = ""
+    for letra in nombre {
+        let letraEnNumero = Int(String(letra)) // Si falla la conversion a número devuelve nil
+        if letraEnNumero != nil {
+            let numeroEscrito = numeros[letraEnNumero!]
+            resultado += numeroEscrito ?? ""
+        } else {
+            resultado += String(letra)
+        }
     }
-} while nombreIncorrecto
+    return resultado
+}
 
+let nombre = cambiarNumeros(nombre: pedirNombre())
 print("Hola", nombre)
-*/
 
-opcionales()
